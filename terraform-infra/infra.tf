f1cb31a072d723afdff4b46fdc8c08328350282f
Sagar Kolli <sagar.kolli@apollointelligence.net>
2025-03-19 00:34:58 +0530

    Terraform code to deploy infrastructure

--- terraform-infra/infra.tf
+++ terraform-infra/infra.tf
@@ -10,9 +10,9 @@ module "vpc" {
   name = "secure-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  azs             = ["us-east-1a", "us-east-1b"] # Added a second AZ
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"] # Added second private subnet
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"] # Added second public subnet
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
@@ -56,7 +56,7 @@ module "eks" {
   cluster_version = "1.27"
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
+  subnet_ids = module.vpc.private_subnets # Using private subnets in two AZs
   enable_irsa = true
 
   eks_managed_node_groups = {
